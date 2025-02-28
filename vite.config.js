85303194573c9611b9595d55a7e4bf9839f0c6bd
unknown <dunyaei@code.edu.az>
2025-02-28 23:52:37 +0400

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+  base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        register: "register.html",
+        login: "login.html",
+        register: "register.html",
+        user: "user.html",
+        buy: "buy.html",
+        events: "events.html",
+        favourites: "favourites.html",
+        popular: "popular.html",
+        tourism: "tourism.html",
+        weekend: "weekend.html",
+      },
+    },
+  },
+});
